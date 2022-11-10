8fffa5df174d30e4dc50966ec8e8f6a72ccd79ff
Jawad Dhew <jawaddhew98@gmail.com>
2022-11-10 09:37:18 +0600

    env file added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDx-tCT38ML6UWDrLJZggeMmf10SHc-XwU",
-    authDomain: "my-kitchen-a41d6.firebaseapp.com",
-    projectId: "my-kitchen-a41d6",
-    storageBucket: "my-kitchen-a41d6.appspot.com",
-    messagingSenderId: "206179223114",
-    appId: "1:206179223114:web:7fd95f631fdbdb328c08b3",
-    measurementId: "G-BWSPWR3SE8"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId,
 };
 
 // Initialize Firebase
